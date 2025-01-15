8cd6bb2f439524dbcf682b007372dd39faf3cd68
AndyFranck23 <ironmanandy23@gmail.com>
2025-01-15 10:23:24 +0300

    mety

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -3,23 +3,23 @@ import path from "path";
 
 // Vos routes React Router
 const routes = [
-    "/", // Accueil
-    "/Home", // Exemple de route supplémentaire
+  "/", // Accueil
+  "/Home", // Exemple de route supplémentaire
 ];
 
 // Générer le contenu du fichier sitemap.xml
 const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${routes
-        .map((route) => {
-            return `
+    .map((route) => {
+      return `
         <url>
-          <loc>https://andy23portfolio.netlify.app${route}</loc>
+          <loc>https://andyfranckportfolio.netlify.app/${route}</loc>
           <priority>0.8</priority>
         </url>
       `;
-        })
-        .join("")}
+    })
+    .join("")}
 </urlset>`;
 
 // Sauvegarder le fichier dans le dossier public
