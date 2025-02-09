19a04c72c0a281441eb9a7d5818e52aaeaff54e5
AndyFranck23 <ironmanandy23@gmail.com>
2025-02-09 17:12:42 +0300

    nextjs3

--- app/layout.js
+++ app/layout.js
@@ -20,10 +20,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" href="/icon.png" />
         <meta name="google-adsense-account" content="ca-pub-3065520205265426" />
         <meta name="google-site-verification" content="SmhbBbx8E73PiiMC2VoGXxVnFl5XQp9Qhz9pNpKabT8" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3065520205265426"
-          crossorigin="anonymous"></script>
+          crossOrigin="anonymous"></script>
       </head>
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
